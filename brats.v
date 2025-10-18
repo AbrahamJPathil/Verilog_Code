4ae0d4ffd0e1b6f904bd841891e26ee5f1f0fc07
Abraham Jacob Pathil <abrahamjpathildev@gmail.com>
2025-10-18 12:07:57 +0530

    Update brats.v
    Added fullAdder as well as integrated it into the existing TestBench (faulty - leads to race conditions in simulation,
    so shall create separate testbenches)

--- brats.v
+++ brats.v
@@ -6,6 +6,16 @@ module halfAdder(input A,B,output C_out,S);
 	and(C_out,A,B);
 endmodule
 
+module fullAdder(input A,B,C_in, output C_out,S);
+	wire and1,and2,or1;
+	xor(S,A,B,C_in);
+	or(or1,A,B);
+	and(C_in,or1);
+	and(and2,A,B);
+	or(C_out,and1,and2);
+endmodule
+
+
 module TestBench;
 	reg A,B;
 	wire C_out,S;
@@ -14,6 +24,7 @@ module TestBench;
 	A = 0; B = 0;
 	$dumpfile("brats.vcd");
 	$dumpvars(0,TestBench);
+	$display(" // Truth Table for Half Adder || ");
 	$display("A | B | C_out | S ");
 	$monitor("%d | %d |   %d | %d ",A,B,C_out,S);
 	#10; A = 0; B = 1;
@@ -21,7 +32,25 @@ module TestBench;
 	#10; A = 1; B = 1;
 	$finish;
 	end
-endmodule
+	
+	reg C_in;
+	fullAdder instance1(A,B,C_in,C_out,S);
+	initial begin
+	A = 0; B = 0;
+	$dumpfile("fullAdder.vcd");
+	$dumpvars(0,TestBench);
+	$display(" // Truth Table for Full Adder || ");
+	$display("A | B | C_in | C_out | S ");
+	$monitor("%d | %d |   %d  |   %d  | %d ",A,B,C_in,C_out,S);
+	#10; A = 0; B = 1;
+	#10; A = 1; B = 0;
+	#10; A = 1; B = 1;
+	$finish;
+	end
+	
+	
+	
+endmodule	
 	 
 	
 	
