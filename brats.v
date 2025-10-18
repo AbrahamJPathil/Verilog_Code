08180e557ef6f1848f2f5d6e4b8d77de6e739bab
Abraham Jacob Pathil <abrahamjpathildev@gmail.com>
2025-10-18 21:21:33 +0530

    Update brats.v
    Successful integration of both testbenches, check the comments

--- brats.v
+++ brats.v
@@ -10,47 +10,43 @@ module fullAdder(input A,B,C_in, output C_out,S);
 	wire and1,and2,or1;
 	xor(S,A,B,C_in);
 	or(or1,A,B);
-	and(C_in,or1);
+	and(and1,C_in,or1);
 	and(and2,A,B);
 	or(C_out,and1,and2);
 endmodule
 
 
-module TestBench;
+module TestBenchHA;
 	reg A,B;
 	wire C_out,S;
 	halfAdder instance0(.A(A),.B(B),.C_out(C_out),.S(S));
+	reg C; //created the additional i/p line for full adder
+	wire D,E;	// created the new o/p lines for full adder to prevent race condition, if prev used
+	fullAdder instance1(.A(A),.B(B),.C_in(C),.C_out(D),.S(E));	
 	initial begin
 	A = 0; B = 0;
 	$dumpfile("brats.vcd");
-	$dumpvars(0,TestBench);
+	$dumpvars(0,TestBenchHA);
 	$display(" // Truth Table for Half Adder || ");
 	$display("A | B | C_out | S ");
 	$monitor("%d | %d |   %d | %d ",A,B,C_out,S);
 	#10; A = 0; B = 1;
 	#10; A = 1; B = 0;
 	#10; A = 1; B = 1;
-	$finish;
-	end
-	
-	reg C_in;
-	fullAdder instance1(A,B,C_in,C_out,S);
-	initial begin
-	A = 0; B = 0;
-	$dumpfile("fullAdder.vcd");
-	$dumpvars(0,TestBench);
 	$display(" // Truth Table for Full Adder || ");
 	$display("A | B | C_in | C_out | S ");
-	$monitor("%d | %d |   %d  |   %d  | %d ",A,B,C_in,C_out,S);
-	#10; A = 0; B = 1;
-	#10; A = 1; B = 0;
-	#10; A = 1; B = 1;
-	$finish;
-	end
-	
-	
-	
+	$monitor("%d | %d | %d | %d | %d",A,B,C,D,E);
+	#10; A = 0; B = 0; C = 0;
+	#10; A = 0; B = 0; C = 1;
+	#10; A = 0; B = 1; C = 0;
+	#10; A = 0; B = 1; C = 1;
+	#10; A = 1; B = 0; C = 0;
+	#10; A = 1; B = 0; C = 1;
+	#10; A = 1; B = 1; C = 0;
+	#10; A = 1; B = 1; C = 1;
+	$finish;		// ends the simulation at all initial blocks, in all modules, enters cleanup mode
+	end	
 endmodule	
 	 
-	
+
 	
