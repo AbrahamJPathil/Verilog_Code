16ff82d042c13f6d5cc5f93af5daad36c51d3315
Abraham Jacob Pathil <abrahamjpathildev@gmail.com>
2025-10-19 12:31:25 +0530

    Invalid module name
    Changed the invalid module name (Syntax error), also added the rest of testBench code

--- mux.v
+++ mux.v
@@ -1,6 +1,6 @@
 //4x1 mux
 
-module 4x1Mux(input L0,L1,L2,L3,S1,S0, output S);
+module _4x1Mux(input L0,L1,L2,L3,S1,S0, output S);
 	wire and0, and1, and2, and3;
 	and(and0,L0,~S1,~S0);
 	and(and1,L1,~S1,S0);
@@ -12,8 +12,30 @@ endmodule
 module TestBench;
 	reg L0,L1,L2,L3,S1,S0;
 	wire S;
-	4x1Mux instance0(.L0(L0),.L1(L1),.L2(L2),.L3(L3),.S1(S1),.S2(S2),.S(S));
+	_4x1Mux instance0(.L0(L0),.L1(L1),.L2(L2),.L3(L3),.S1(S1),.S2(S2),.S(S));
 	initial begin
+	$dumpfile("mux.vcd");
+	$dumpvars(0,TestBench);
+	L0 = 0;L1 = 0;L2 = 0;L3 = 0;
+	S0 = 0; S1 = 0;
+	$display("L0 | L1 | L2 | L3 | S1 | S0");
+	$monitor("%d | %d | %d | %d | %d | %d ",L0,L1,L2,L3,S1,S0);
+	#10; L0 = 1;L1 = 0;L2 = 0;L3 = 0; S1 = 0; S0 = 0;
+	
+	#10; L0 = 0;L1 = 0;L2 = 0;L3 = 0; S1 = 0; S0 = 1;
+	#10; L0 = 0;L1 = 1;L2 = 0;L3 = 0; S1 = 0; S0 = 1;
+	
+	#10; L0 = 0;L1 = 0;L2 = 0;L3 = 0; S1 = 1; S0 = 0;
+	#10; L0 = 0;L1 = 0;L2 = 1;L3 = 0; S1 = 1; S0 = 0;
+	
+	#10; L0 = 0;L1 = 0;L2 = 0;L3 = 0; S1 = 1; S0 = 1;
+	#10; L0 = 0;L1 = 0;L2 = 0;L3 = 1; S1 = 1; S0 = 1;
+	
+	#10; $monitoroff
+	$finish
+	end
+endmodule
+	
 	
 	
 	
