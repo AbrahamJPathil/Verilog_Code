e1a01daf9ae28c91fb972bc31db213da1b8861c6
Abraham Jacob Pathil <abrahamjpathildev@gmail.com>
2025-10-28 09:52:43 +0530

    Update latchesNffs.v
    Line 3 --> added the NOT gate instance QBar so that it gets initialised as complement of Q
    
    Line 10 --> Corrected syntax error with instantiation
    
    Line 14 --> began with a known test case else uninitialized Q & Qbar will cause issues

--- latchesNffs.v
+++ latchesNffs.v
@@ -1,21 +1,22 @@
-module srLatch(input S,R, reg output Q,QBar);
-	nor(Q,Qbar,R);
-	nor(Qbar,S,Q);
+module srLatch(input S,R, output Q,QBar);
+	nor n1(Q,Qbar,R);
+	not(QBar,Q);
+	nor n2(Qbar,S,Q);
 endmodule
 
 module testBench;
 	reg S,R;
 	wire Q,Qbar;
-	srLatch instance0(.S(S),.R(R),.Q(Q),.Qbar(Qbar));
+	srLatch instance0(.S(S),.R(R),.Q(Q),.QBar(Qbar));
 	initial begin
 	$dumpfile("latchesNffs.vcd");
 	$dumpvars(0,testBench);
-	S = 0; R = 0;
+	S = 0; R = 1;
 	$display("S | R | Q | Qbar");
 	$monitor("%d | %d | %d | %d",S,R,Q,Qbar);
-	#10; S = 0; R = 1;
 	#10; S = 1; R = 0;
+	#10; S = 0; R = 0;
 	#10; S = 0; R = 1;
-	#10; $finish
+	#10; $finish;
 	end
 endmodule	
